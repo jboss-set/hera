a9a0a2e972bdf4c1e95e79ff54a3cbe086471022
Romain Pelisse <belaran@gmail.com>
2022-09-02 18:51:43 +0200

    TOREWORD

--- stop.sh
+++ stop.sh
@@ -7,4 +7,9 @@ source "${HERA_HOME}"/library.sh
 
 readonly CONTAINER_TO_DELETE=$(container_name "${JOB_NAME}" "${BUILD_ID}")
 is_defined "${CONTAINER_TO_DELETE}" "Could not generate container name with provided JOB_NAME(${JOB_NAME}) and BUILD_ID(${BUILD_ID}."
+if [[ "${CONTAINER_TO_DELETE}" == *"-slave-"* ]]; then
+  echo "${CONTAINER_TO_DELETE} is a molecule slave running with systemd, so trying halt first:"
+  run_ssh "podman exec ${CONTAINER_TO_DELETE} '/usr/sbin/halt'"
+  sleep 2
+fi
 run_ssh "podman stop -i ${CONTAINER_TO_DELETE}"
