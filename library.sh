56d86384a8a6c0627abcda4f61611af36fe96002
Romain Pelisse <belaran@gmail.com>
2022-09-05 14:08:30 +0200

    reword

--- library.sh
+++ library.sh
@@ -5,6 +5,7 @@ readonly HERA_HOSTNAME=${HERA_HOSTNAME:-'podman.host'}
 readonly HERA_USERNAME=${HERA_USERNAME:-'jenkins'}
 readonly HERA_SSH_OPTIONS=${HERA_SSH_OPTIONS}
 readonly CONTAINER_NAME_PREFIX=${CONTAINER_NAME_PREFIX:-'automaton-slave'}
+readonly CONTAINER_COMMAND=${CONTAINER_COMMAND}
 export TERM=${TERM:-'screen'}
 set -u
 
