398af5b87aedb92a6f623259c92bcbb2e4e2a58d
Romain Pelisse <belaran@gmail.com>
2024-02-06 16:18:36 +0100

    add timeout to kill dead containers and avoid them lingering

--- stop.sh
+++ stop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 readonly HERA_HOME=${HERA_HOME}
+readonly TIMEOUT_BEFORE_KILLING_CONTAINER=${TIMEOUT_BEFORE_KILLING_CONTAINER:-'15'}
 set -euo pipefail
 
 # shellcheck source=library.sh
@@ -7,4 +8,4 @@ source "${HERA_HOME}"/library.sh
 
 readonly CONTAINER_TO_DELETE=$(container_name "${JOB_NAME}" "${BUILD_ID}")
 is_defined "${CONTAINER_TO_DELETE}" "Could not generate container name with provided JOB_NAME(${JOB_NAME}) and BUILD_ID(${BUILD_ID}."
-run_ssh "podman stop -i ${CONTAINER_TO_DELETE}"
+run_ssh "podman stop -t ${TIMEOUT_BEFORE_KILLING_CONTAINER} -i ${CONTAINER_TO_DELETE}"
